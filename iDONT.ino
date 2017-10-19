e4e38bd27e516eb2270442b7a343a2b09e701d4d
Joel Whitehouse <code@joelwhitehouse.com>
2017-10-19 01:33:32 -0500

    HAL routines

--- iDONT.ino
+++ iDONT.ino
@@ -1,22 +1,35 @@
 #include "mainsm.h"
 #include "ledsm.h"
+#include "nvram.h"
 #include <DNSServer.h>            //Local DNS Server used for redirecting all requests to the configuration portal
 #include <ESP8266WebServer.h>     //Local WebServer used to serve the configuration portal
 #include <WiFiManager.h>          //https://github.com/kentaylor/WiFiManager WiFi 
 #include <ESP8266WebServer.h>
 
+
 WiFiServer* pWifiServer;
 unsigned char mainState;
 unsigned char ledState;
-void setup() {
+
+void setup() {  
   // put your setup code here, to run once:
+  initialize_hal();
+  nvram_init();
   Serial.begin(115200);
 
   mainState = STATE_MAIN_INIT;
   ledState= STATE_LED_INIT_OFF;
-  pinMode(ESP8266_LED, OUTPUT);
+  pinMode(MAIN_LED, OUTPUT);
   
-  delay(4000);
+  enable_bell();
+  delay(1000);
+  disable_bell();
+  delay(1000);
+  enable_bell();
+  delay(1000);
+  disable_bell();
+  delay(1000);
+  nvram_dump();
 }
 
 void loop() 
--- mainsm.h
+++ mainsm.h
@@ -4,6 +4,7 @@
 #include <ESP8266WiFi.h>          //ESP8266 Core WiFi Library
 #include <ESP8266WebServer.h>
 
+
 #define STATE_MAIN_INIT                  (0)
 #define STATE_MAIN_START_CAPTIVE_PORTAL  (1)
 #define STATE_MAIN_INIT_HTTP             (2)
@@ -33,7 +34,7 @@ void serve_wifi_client(WiFiServer* pServer)
 {
   WiFiClient client = pServer->available();
   char linebuf[80];
-  int charcount=0;
+  unsigned int charcount=0;
 
   if (client) {
   Serial.println("New client");
